56858e48d4a8870753d2f5c683c2fc66784882e9
UCDM <palama.ucdm.1212@gmail.com>
2022-01-12 23:11:44 +0900

    コメントフォームを追加

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,6 +8,7 @@ import { Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import MessageIcon from "@material-ui/icons/Message";
 import SendIcon from "@material-ui/icons/Send";
+import { url } from "inspector";
 
 interface PROPS {
   postId: string;
@@ -20,6 +21,21 @@ interface PROPS {
 
 export const Post: React.FC<PROPS> = (props) => {
   const { postId, avatar, image, text, timestamp, username } = props;
+
+  const user = useSelector(selectUser);
+
+  const [comment, setComment] = useState("");
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection("posts").doc(postId).collection("comments").add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
+
   return (
     <div className={styles.post}>
       <div className={styles.post_avater}>
@@ -39,6 +55,28 @@ export const Post: React.FC<PROPS> = (props) => {
                 <img src={props.image} alt="tweet" />
               </div>
             )}
+            <form onSubmit={newComment}>
+              <div className={styles.post_form}>
+                <input
+                  className={styles.post_input}
+                  type="text"
+                  placeholder="Type new comment..."
+                  value={comment}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setComment(e.target.value)
+                  }
+                />
+                <button
+                  disabled={!comment}
+                  className={
+                    comment ? styles.post_button : styles.post_buttonDisable
+                  }
+                  type="submit"
+                >
+                  <SendIcon className={styles.post_sendIcon} />
+                </button>
+              </div>
+            </form>
             <div className={styles.post_tweet}>
               <p>{text}</p>
             </div>
